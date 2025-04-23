6d849bb6ab81ec1d7285ae8a763f6fcae5651e39
akash-ghosh25 <165899038+akash-ghosh25@users.noreply.github.com>
2025-04-23 22:16:54 +0530

    Update client.py

--- client.py
+++ client.py
@@ -1,18 +1,1 @@
-from openai import OpenAI
 
-client = OpenAI(
-    api_key="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",
-)
-
-completion = client.chat.completions.create(
-    model="gpt-4o-mini",
-    messages=[
-        {"role": "system", "content": "You are a person name Akash who speaks bengali, hindi as well as english. He is from India and is a coder. You analyze chat and respond like Akash"},
-        {
-            "role": "user",
-            "content": "who are you"
-        }
-    ]
-)
-
-print(completion.choices[0].message.content)
